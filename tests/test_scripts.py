5ddce2dc19c3f7ccb7da8e2573b0a3e0346e4772
Alex Drozd <drozdster@gmail.com>
2022-07-11 09:56:33 -0500

    adding functionality for duplicating stages

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -1,11 +1,16 @@
 import json
+import os
 
 from moto import mock_iam
 
+from kicksaw_aws_iam_policy_management.dupe import duplicate_stage
 from kicksaw_aws_iam_policy_management.script import sync_iam
 
 USER_ARN = "arn:aws:iam::123456789012:user/example-project-cicd"
 POLICY_ARN = "arn:aws:iam::123456789012:policy/example-project-deploy"
+
+STAGE = os.getenv("STAGE")
+TARGET_STAGE = "production"
 
 
 @mock_iam
@@ -16,8 +21,21 @@ def test_sync_iam():
     with open("config-iam.json") as file:
         data = json.load(file)
 
-    assert data["stages"]["test"]["arn"] == USER_ARN
-    assert data["policies"][0]["stages"]["test"]["arn"] == POLICY_ARN
+    assert data["stages"][STAGE]["arn"] == USER_ARN
+    assert data["policies"][0]["stages"][STAGE]["arn"] == POLICY_ARN
 
     # then just make sure it runs again
     sync_iam()
+
+    # and check the stage duplication stuff works
+    duplicate_stage(STAGE, "production")
+
+    with open("config-iam.json") as file:
+        data = json.load(file)
+
+    # check the original is still there
+    assert data["stages"][STAGE]["arn"] == USER_ARN
+    assert data["policies"][0]["stages"][STAGE]["arn"] == POLICY_ARN
+    # check the target got copied
+    assert data["stages"][TARGET_STAGE]["arn"] == USER_ARN
+    assert data["policies"][0]["stages"][TARGET_STAGE]["arn"] == POLICY_ARN
